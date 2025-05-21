ecb75a90f677919979c0dd22ebc0803378a003cb
Ramchani Fares <150725072+fares-ramchani@users.noreply.github.com>
2025-05-21 17:51:30 +0100

    Update service.ts

--- src/core/services/axios/service.ts
+++ src/core/services/axios/service.ts
@@ -1,7 +1,7 @@
 import { Api } from '../mapping/index';
 
 export const service = new Api({
-  baseUrl: `${import.meta.env.VITE_API_BASE_URL}`,
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
   baseApiParams: {
     credentials: 'include',
   },
