22a3e6f1334426b3c3024066816b9c6d170590c0
Ramchani Fares <150725072+fares-ramchani@users.noreply.github.com>
2025-05-21 19:05:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
@@ -6,38 +6,42 @@ import tailwind from 'tailwindcss';
 import * as path from 'path';
 import svgLoader from 'vite-svg-loader';
 
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
-  plugins: [
-    vue(),
-    svgLoader(),
-    eslintPlugin({
-      cache: false,
-      include: ['src/**/*.ts', 'src/**/*.vue'],
-      exclude: ['node_modules'],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/rms/api': {
-        target: process.env.VITE_API_BASE_URL, // Proxy to the backend server
-        changeOrigin: true, // Ensures that the Host header is correctly set
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
       },
     },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
+    plugins: [
+      vue(),
+      svgLoader(),
+      eslintPlugin({
+        cache: false,
+        include: ['src/**/*.ts', 'src/**/*.vue'],
+        exclude: ['node_modules'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/rms/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+  };
 });
